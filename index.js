c03d02ac2b429e413e59f338266b1621e3e491d7
paulhorowitz <paulbhorowitz@gmail.com>
2020-12-04 14:18:27 +1030

    fix grid resize issues

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-gridSquares = 16;
+gridSquares = 64;
 const container = document.getElementById('container');
+container.style.gridTemplateColumns = `repeat(${gridSquares}, ${(600/gridSquares)}px)`;
+container.style.gridTemplateRows = `repeat(${gridSquares}, ${(600/gridSquares)}px)`;
 console.log(container)
-for (i = 0; i < gridSquares * gridSquares; i++) {
-    const div = document.createElement('div');
-    container.appendChild(div);
-    div.classList.add('div')
-    div.style.gridTemplateColumns = `repeat(${gridSquares}, 25px)`;
-    div.style.gridTemplateRows = `repeat(${gridSquares}, 25px)`;
+
+function createGrid() {
+    for (i = 0; i < gridSquares * gridSquares; i++) {
+        const div = document.createElement('div');
+        container.appendChild(div);
+        div.classList.add('div')
+    }
 }
+
+createGrid(gridSquares)
