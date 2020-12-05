d22b31a4c246853e41f419262e100e9bbcc551bc
paulhorowitz <paulbhorowitz@gmail.com>
2020-12-05 11:01:33 +1030

    Add rainbow effect

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-gridSquares = 64;
+gridSquares = 16;
 const container = document.getElementById('container');
-container.style.gridTemplateColumns = `repeat(${gridSquares}, ${(600/gridSquares)}px)`;
-container.style.gridTemplateRows = `repeat(${gridSquares}, ${(600/gridSquares)}px)`;
+const containerGridSize = 800;
+container.style.width = `${containerGridSize}px`;
+container.style.height = `${containerGridSize}px`;
+container.style.gridTemplateColumns = `repeat(${gridSquares}, ${(containerGridSize/gridSquares)}px)`;
+container.style.gridTemplateRows = `repeat(${gridSquares}, ${(containerGridSize/gridSquares)}px)`;
+currentColor = [];
 console.log(container)
 
 function createGrid() {
     for (i = 0; i < gridSquares * gridSquares; i++) {
         const div = document.createElement('div');
         container.appendChild(div);
-        div.classList.add('div')
+        div.classList.add('div');
     }
 }
 
+function randomColor() {
+    currentColor[0] = Math.floor(Math.random() * 255);
+    currentColor[1] = Math.floor(Math.random() * 255);
+    currentColor[2] = Math.floor(Math.random() * 255);
+    return (currentColor)
+}
+
 createGrid(gridSquares)
+const squares = document.querySelectorAll('div');
+currentColor = randomColor()
+console.log(currentColor)
+squares.forEach(element => {
+    element.addEventListener('mouseover', function(event) {
+        currentColor = randomColor()
+        event.target.style.backgroundColor = `rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
+    })
+});
