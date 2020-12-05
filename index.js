090d9ca5b735a75ba372e64f2b14c696a5df9fe5
paulhorowitz <paulbhorowitz@gmail.com>
2020-12-05 13:05:36 +1030

    add black button

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 gridSquares = 16;
+currentColor = [];
 const container = document.getElementById('container');
 const containerGridSize = 800;
-const squares = document.querySelectorAll('div');
 container.style.width = `${containerGridSize}px`;
 container.style.height = `${containerGridSize}px`;
 container.style.gridTemplateColumns = `repeat(${gridSquares}, ${(containerGridSize/gridSquares)}px)`;
 container.style.gridTemplateRows = `repeat(${gridSquares}, ${(containerGridSize/gridSquares)}px)`;
-currentColor = [];
-const rainbowButton = document.getElementById('rainbow-button')
+const rainbowButton = document.getElementById('rainbow-button');
+const blackButton = document.getElementById('black-button');
 
 function createGrid() {
     for (i = 0; i < gridSquares * gridSquares; i++) {
         const div = document.createElement('div');
+        div.classList.add('grid');
         container.appendChild(div);
-        div.classList.add('div');
         div.style.backgroundColor = 'white';
         createHoverEffect();
     }
@@ -28,7 +28,7 @@ function randomColor() {
 
 function rainbowColors() {
     currentColor = randomColor()
-    squares.forEach(element => {
+    document.querySelectorAll('div.grid').forEach(element => {
         element.addEventListener('mouseover', function(event) {
             currentColor = randomColor()
             event.target.style.backgroundColor = `rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
@@ -36,8 +36,9 @@ function rainbowColors() {
     });
 }
 
+
 function createHoverEffect() {
-    squares.forEach(element => {
+    document.querySelectorAll('div.grid').forEach(element => {
         element.addEventListener('mouseover', function(event) {
             event.target.style.backgroundColor = 'black';
         })
@@ -46,5 +47,8 @@ function createHoverEffect() {
 
 createGrid(gridSquares)
 rainbowButton.addEventListener('click', function (e) {
-    rainbowColors()
+    rainbowColors();
+})
+blackButton.addEventListener('click', function (e) {
+    createHoverEffect();
 })
