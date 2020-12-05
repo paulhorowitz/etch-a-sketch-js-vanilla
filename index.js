bea8cbb494158a0f851b5ea39413627b763c4fb2
paulhorowitz <paulbhorowitz@gmail.com>
2020-12-05 12:15:57 +1030

    add CSS and rainbow button

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 gridSquares = 16;
 const container = document.getElementById('container');
 const containerGridSize = 800;
+const squares = document.querySelectorAll('div');
 container.style.width = `${containerGridSize}px`;
 container.style.height = `${containerGridSize}px`;
 container.style.gridTemplateColumns = `repeat(${gridSquares}, ${(containerGridSize/gridSquares)}px)`;
 container.style.gridTemplateRows = `repeat(${gridSquares}, ${(containerGridSize/gridSquares)}px)`;
 currentColor = [];
-console.log(container)
+const rainbowButton = document.getElementById('rainbow-button')
 
 function createGrid() {
     for (i = 0; i < gridSquares * gridSquares; i++) {
         const div = document.createElement('div');
         container.appendChild(div);
         div.classList.add('div');
+        div.style.backgroundColor = 'white';
+        createHoverEffect();
     }
 }
 
@@ -23,13 +26,25 @@ function randomColor() {
     return (currentColor)
 }
 
-createGrid(gridSquares)
-const squares = document.querySelectorAll('div');
-currentColor = randomColor()
-console.log(currentColor)
-squares.forEach(element => {
-    element.addEventListener('mouseover', function(event) {
-        currentColor = randomColor()
-        event.target.style.backgroundColor = `rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
+function rainbowColors() {
+    currentColor = randomColor()
+    squares.forEach(element => {
+        element.addEventListener('mouseover', function(event) {
+            currentColor = randomColor()
+            event.target.style.backgroundColor = `rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`
+        })
+    });
+}
+
+function createHoverEffect() {
+    squares.forEach(element => {
+        element.addEventListener('mouseover', function(event) {
+            event.target.style.backgroundColor = 'black';
+        })
     })
-});
+}
+
+createGrid(gridSquares)
+rainbowButton.addEventListener('click', function (e) {
+    rainbowColors()
+})
