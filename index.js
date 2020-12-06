e53300db31023e0acce7ebaf0d12784cacaad98f
paulhorowitz <paulbhorowitz@gmail.com>
2020-12-06 16:05:10 +1030

    Added grid slider and kids images

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-gridSquares = 32;
+gridSquares = 7;
 currentColor = [];
 const container = document.getElementById('container');
 const containerGridSize = 800;
 container.style.width = `${containerGridSize}px`;
 container.style.height = `${containerGridSize}px`;
-container.style.gridTemplateColumns = `repeat(${gridSquares}, ${(containerGridSize/gridSquares)}px)`;
-container.style.gridTemplateRows = `repeat(${gridSquares}, ${(containerGridSize/gridSquares)}px)`;
 const rainbowButton = document.getElementById('rainbow-button');
 const blackButton = document.getElementById('black-button');
 const resetButton = document.getElementById('reset-button');
@@ -13,14 +11,16 @@ const toggleGridEl = document.getElementById('toggle-grid');
 var gridOn = true;
 
 function createGrid(gridSquares) {
+    container.style.gridTemplateColumns = `repeat(${gridSquares}, ${(containerGridSize/gridSquares)}px)`;
+    container.style.gridTemplateRows = `repeat(${gridSquares}, ${(containerGridSize/gridSquares)}px)`;
     for (i = 0; i < gridSquares * gridSquares; i++) {
         const div = document.createElement('div');
         div.classList.add('grid');
         container.appendChild(div);
         div.style.backgroundColor = 'white';
-        createHoverEffect();
         div.style.border = '1px solid #40abbf'
     }
+    createHoverEffect();
 }
 
 // Creates an array of random RGB color numbers
@@ -75,6 +75,14 @@ function toggleGrid() {
     }
 }
 
+function removeGrid() {
+    const container = document.getElementById("container");
+    while (container.firstChild) {
+        console.log('Test')
+        container.removeChild(container.lastChild);
+    }
+}
+
 createGrid(gridSquares)
 rainbowButton.addEventListener('click', function (e) {
     rainbowColors();
@@ -90,3 +98,15 @@ resetButton.addEventListener('click', function (e) {
 toggleGridEl.addEventListener('click', function (e) {
     toggleGrid()
 })
+
+const slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    gridValue = this.value;
+    removeGrid();
+    createGrid(gridValue);
+}
+
